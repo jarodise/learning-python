1dcde5e07b608814f2f3ece5eb589862383dd8a5
jarodise <jarodise@gmail.com>
2022-07-08 11:56:00 +0300

    Create list1.py

---
+++ list1.py
@@ -0,0 +1,9 @@
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+# Define your code below:
+result = ""
+i = 0
+while i < len(sounds):
+    result = result + sounds[i]
+    i += 1 
+print(result.upper())    
